c59abab9050805058d3c1e56b7ae4d750861353e
hana-sama <hmama8989@gmail.com>
2020-09-18 08:09:17 +0900

    useEffect

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,22 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 
 function App(props) {
   const [state, setState] = useState(props)
   const {name, price} = state
   const reset = ()=> setState(props)
+  useEffect(()=>{
+    console.log("This function called only when name parameter is changed")
+  },[name])
+  useEffect(()=>{
+    console.log("This function called only when price parameter is changed")
+  },[price])
+  useEffect(()=>{
+    console.log("This function called only when initial DOM is rendered")
+  },[])
+  useEffect(()=>{
+    console.log("This function function almost like componentDidMount or componentUpdate")
+  })
+
   return (
     <React.Fragment>
       <p>現在{name}のお値段は、{price}円です。</p>
