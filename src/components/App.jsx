742264e9c289ea8456fc6d108e88e9fc5d7b38a3
hana-sama <hmama8989@gmail.com>
2020-09-18 07:09:49 +0900

    multiple-states

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,69 +1,29 @@
 import React, {useState} from 'react'
 
-function App() {
-  const initialState = 0
-  const [count, setCount] = useState(0)
-  function increment(){
-    setCount(count +1)
-  }
-  function decrement(){
-    setCount(count -1)
-  }
-  function reset(){
-    setCount(initialState)
-  }
-  function increment2(){
-    setCount((prevValue)=>{
-      return prevValue +10
-    })
-  }
-  function decrement2(){
-    setCount((prevValue)=>{
-      return prevValue -10
-    })
-  }
-  function multiply(){
-    setCount((prevValue)=>{
-      return prevValue *2
-    })
-  }
-  function divide(){
-    setCount((prevValue)=>{
-      return prevValue / 2
-    })
-  }
-  function modulo(){
-    setCount((prevValue)=>{
-      if(prevValue % 3 === 0){
-        return prevValue / 3
-      } else {
-        return "3の倍数ではありません！"
-      }
-    })
+function App(props) {
+   const [name, setName] = useState(props.name)
+  const [price, setPrice] = useState(props.price)
+  const reset = ()=>{
+    setPrice(props.price)
+    setName(props.name)
   }
   return (
     <React.Fragment>
-      <h1>count: {count}</h1>
-      <div>
-      <button onClick={increment}>+1</button>
-      <button onClick={decrement}>-1</button>
-      </div>
-      <div>
-        <button onClick={increment2}>+10</button>
-        <button onClick={decrement2}>-10</button>
-      </div>
-      <div>
+      <p>現在{name}のお値段は、{price}円です。</p>
+      <button onClick={()=> setPrice(prevPrice => prevPrice+10)}>+10</button>
+      <button onClick={()=> setPrice(prevPrice => prevPrice -10)}>-10</button>
       <button onClick={reset}>Reset</button>
-      </div>
-      <div>
-        <button onClick={multiply}>*2</button>
-        <button onClick={divide}>/2</button>
-      </div>
-      <div>
-        <button onClick={modulo}>3の倍数の時だけ３で割る</button>
-      </div>
+      <input 
+      value={name}
+      onChange={e => setName(e.target.value)}
+      />
     </React.Fragment>
   )
+}
+
+App.defaultProps = {
+  name:"",
+  price: 1000
 }
 
 export default App
