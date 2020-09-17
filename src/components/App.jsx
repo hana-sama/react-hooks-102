57c96824c4d5a8958d2e3bcd2682d82abf49174c
hana-sama <hmama8989@gmail.com>
2020-09-18 07:34:22 +0900

    implemented object states

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,18 @@
 import React, {useState} from 'react'
 
 function App(props) {
-   const [name, setName] = useState(props.name)
-  const [price, setPrice] = useState(props.price)
-  const reset = ()=>{
-    setPrice(props.price)
-    setName(props.name)
-  }
+  const [state, setState] = useState(props)
+  const {name, price} = state
+  const reset = ()=> setState(props)
   return (
     <React.Fragment>
       <p>現在{name}のお値段は、{price}円です。</p>
-      <button onClick={()=> setPrice(prevPrice => prevPrice+10)}>+10</button>
-      <button onClick={()=> setPrice(prevPrice => prevPrice -10)}>-10</button>
+      <button onClick={()=> setState({...state, price:price +10})}>+10</button>
+      <button onClick={()=> setState({...state, price:price -10})}>-10</button>
       <button onClick={reset}>Reset</button>
       <input 
       value={name}
-      onChange={e => setName(e.target.value)}
+      onChange={e => setState({...state, name: e.target.value})}
       />
     </React.Fragment>
   )
