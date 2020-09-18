841ecf62ab9336c979d12ef055eb698abba09ca4
hana-sama <hmama8989@gmail.com>
2020-09-18 09:11:54 +0900

    create event form and event table with bootstrap

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,35 @@
 import React, {useState} from 'react'
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
-  const [count, setCount] = useState(0)
-  function increment(){
-    setCount(count +1)
-  }
-  function decrement(){
-    setCount(count -1)
-  }
-  return (
-    <div>
-      <h1>count: {count}</h1>
-      <button onClick={increment}>+</button>
-      <button onClick={decrement}>-</button>
+   return (
+    <div className="container-fluid">
+      <h4>イベント作成フォーム</h4>
+      <form>
+        <div className="form-group">
+          <label htmlFor="formEventTitle">タイトル</label>
+          <input className="form-control" id="formEventTitle" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="formEventBody">ボディー</label>
+          <textarea className="form-control" id="formEventBody" />
+        </div>
+        <button className="btn btn-primary">イベントを作成する</button>
+        <button className="btn btn-danger">全てのイベントを削除する</button>
+        <button className="btn btn-danger">全ての操作ログを削除する</button>
+      </form>
+      <h4>イベント一覧</h4>
+      <table className="table table-hover">
+        <thead>
+        <tr>
+          <th>ID</th>
+          <th>タイトル</th>
+          <th>ボディ</th>
+          <th></th>
+        </tr>
+        </thead>
+        <tbody>
+        </tbody>
+      </table>
     </div>
   )
 }
