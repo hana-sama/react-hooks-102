31e4e34077b711d59f2a557a0d26ab4510341585
hana-sama <hmama8989@gmail.com>
2020-09-18 09:50:39 +0900

    adding events using reducer

---
+++ src/components/reducers/index.jsx
@@ -0,0 +1,24 @@
+//  action ={
+// type: "CREATE_EVENT",
+// title: "2020東京オリンピック延期のお知らせ"
+//body: "2020年東京オリンピックは新型コロナウイルスの影響により延期されました！つきましては・・・"
+//}
+const events = (state=[], action)=>{
+  switch(action.type){
+    case "CREATE_EVENT":
+      const event ={
+        title: action.title,
+        body: action.body
+      }
+      const length = state.length
+      const id = length === 0 ? 1 : state[length -1].id + 1
+      return [...state, {id, ...event}]
+    case "DELETE_EVENT":
+      return
+    case "DELETE_ALL_EVENTS":
+      return []
+    default:
+      return state
+  }
+}
+export default events;
